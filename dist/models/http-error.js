b9b7119c8822e6e15a6614f540a41e94067eeaaf
Tamtox <39272747@protonmail.com>
2023-01-20 09:23:35 +0300

    asd

--- dist/models/http-error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class HttpError extends Error {
-    constructor(message, errCode) {
-        super(message);
-        this.code = errCode;
-    }
-}
-module.exports = HttpError;
-//# sourceMappingURL=http-error.js.map
